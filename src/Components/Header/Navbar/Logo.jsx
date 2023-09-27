c8599e7d5f76c405c803db9c22fd4f9bccd241ba
sanjidatanha09 <tanha.sanjida999@gmail.com>
2023-09-27 23:53:53 +0600

    add

--- src/Components/Header/Navbar/Logo.jsx
+++ src/Components/Header/Navbar/Logo.jsx
@@ -4,7 +4,7 @@ const Logo = () => {
     return (
         <div>
            
-            <img className="w-[200px] md:w-[150px] lg:w-[200px] md:w-[200px] lg:w-[250px] mx-auto mb-10 md:mb-0 lg:mb-0" src="/public/Resources/Logo.png" alt="" />
+            <img className="w-[200px] md:w-[150px] lg:w-[200px] md:w-[200px] lg:w-[250px] mx-auto mb-10 md:mb-0 lg:mb-0" src="/Resources/Logo.png" alt="" />
         </div>
     );
 };
